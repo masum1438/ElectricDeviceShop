a5098d77a67d7ae95369cd46b410457920b776dd
masum1438 <masum1438@gmail.com>
2025-04-21 16:54:02 +0600

    Add project files.

---
+++ Models/ResetAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace pracLogin.Models
+{
+    public class ResetAccount
+    {
+        public bool VerifyUser2(string txtUsername, string newPassword)
+        {
+            bool status = false;
+            string connString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(connString);
+
+            try
+            {
+                sqlConnection.Open();
+                string query = "UPDATE OSTMember SET Password = @NewPassword WHERE name = @UserName";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.CommandType = CommandType.Text;
+
+                // Add parameters to avoid SQL injection
+                cmd.Parameters.AddWithValue("@NewPassword", newPassword);
+                cmd.Parameters.AddWithValue("@UserName", txtUsername);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    status = true; // Password successfully updated
+                }
+
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+
+            return status;
+        }
+    }
+}
