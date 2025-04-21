716cc1620f4a1effcd4e9c6125be53d826fc5c4c
masum1438 <masum1438@gmail.com>
2025-04-21 19:56:46 +0600

    download and add JqueryUi folder in scripts section thn modal working properlyedit and delete added

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pracLogin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,99 @@ namespace pracLogin.Controllers
         // GET: Inventory
         public ActionResult Dashboard()
         {
+            if (Session["Message"] == null)
+                Session["Message"] = "";
+            BaseEquipment baseEquipment = new BaseEquipment();
+            List<BaseEquipment> equipments = baseEquipment.LstEquipment();
+
+            ViewBag.Equipment = equipments;
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult Dashboard(FormCollection formCollection)
+        {
+            int hdSelectedEquipmentID = Convert.ToInt32(formCollection["hdSelectedEquipmentID"].ToString());
+            string hdSelectedOpType = formCollection["hdSelectedOpType"].ToString();
+
+            BaseEquipment baseEquipmentNew = new BaseEquipment();
+            baseEquipmentNew.EquipmentId = hdSelectedEquipmentID;
+            baseEquipmentNew.EquipmentName = formCollection["txtName"].ToString();
+            baseEquipmentNew.Quantity = Convert.ToInt32(formCollection["txtQuantity"].ToString());
+            baseEquipmentNew.EntryDate = DateTime.ParseExact(formCollection["txtEntryDate"].ToString(), "dd/MM/yyyy", null);
+            baseEquipmentNew.ReceiveDate = DateTime.ParseExact(formCollection["txtReceiveDate"].ToString(), "dd/MM/yyyy", null);
+            if (hdSelectedOpType != "Delete" && hdSelectedOpType != "Update")
+            {
+                if (baseEquipmentNew.SaveEquipment())
+                {
+                    Session["Message"] = "Save Successfully";
+                }
+                else
+                {
+                    Session["Message"] = "Error Occured";
+                }
+            }
+            else
+            {
+                try
+                {
+                    baseEquipmentNew.UpdateEquipment(hdSelectedOpType);
+                    Session["Message"] = hdSelectedOpType == "Delete" ? "Deleted Successfully" : "Updated Successfully";
+                }
+                catch (Exception ex)
+                {
+                    Session["Message"] = ex.Message;
+                }
+
+            }
+
+            BaseEquipment baseEquipment = new BaseEquipment();
+            List<BaseEquipment> equipments = baseEquipment.LstEquipment();
+
+            ViewBag.Equipment = equipments;
+            return View();
+        }
+        public ActionResult CustomerAssignment()
+        {
+            if (Session["Message"] == null)
+                Session["Message"] = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CustomerAssignment(FormCollection frmColl)
+        {
+
+
+
+            BaseEquipment baseEquipment = new BaseEquipment();
+            int ddlCustomer = Convert.ToInt32(frmColl["ddlCustomer"].ToString());
+            int ddlEquipment = Convert.ToInt32(frmColl["ddlEquipment"].ToString());
+            int txtEquiCount = Convert.ToInt32(frmColl["txtEquiCount"].ToString());
+
+            List<BaseEquipment> LstEquipment = baseEquipment.LstEquipment();
+
+            var instock = (from a in LstEquipment where a.EquipmentId == ddlEquipment select a.Stock).FirstOrDefault();
+
+            bool status = true;
+            if (instock != null)
+            {
+                if (txtEquiCount > Convert.ToInt32(instock))
+                {
+                    status = false;
+                }
+            }
+            //select top 1 a.Stock from table where EquipmentId=@EquipmentId
+            if (status)
+            {
+                baseEquipment.SaveCustomerEquipmentAssignment(frmColl);
+                Session["Message"] = "Save Successfully";
+                return RedirectToAction("Dashboard", "Inventory");
+            }
+            else
+            {
+                Session["Message"] = "Requested quantity not available";
+                return View();
+            }
         }
     }
 }
--- Models/BaseEquipment.cs
+++ Models/BaseEquipment.cs
@@ -1,11 +1,209 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace pracLogin.Models
 {
     public class BaseEquipment
     {
+        public int EquipmentId { get; set; }
+        public string EquipmentName { get; set; }
+        public int Quantity { get; set; }
+        public int Stock { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime ReceiveDate { get; set; }
+
+        public List<BaseEquipment> LstEquipment()
+        {
+            List<BaseEquipment> lstEquips = new List<BaseEquipment>();
+
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            try
+            {
+                sqlConnection.Open();
+                string Query = "spOST_LstEquipment";
+                SqlCommand cmd = new SqlCommand(Query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.CommandTimeout = 0;
+
+                //cmd.ExecuteNonQuery();//insert delete update 
+                SqlDataReader reader = cmd.ExecuteReader(); //fetch mode list data 
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        BaseEquipment baseEquipment = new BaseEquipment();
+                        baseEquipment.EquipmentId = Convert.ToInt32(reader["EquipmentId"].ToString());
+                        baseEquipment.EquipmentName = reader["EquipmentName"].ToString();
+                        baseEquipment.Quantity = Convert.ToInt32(reader["Quantity"].ToString());
+                        baseEquipment.Stock = Convert.ToInt32(reader["Stock"].ToString());
+                        baseEquipment.EntryDate = Convert.ToDateTime(reader["EntryDate"].ToString());
+                        baseEquipment.ReceiveDate = Convert.ToDateTime(reader["ReceiveDate"].ToString());
+                        lstEquips.Add(baseEquipment);
+                    }
+                }
+
+                //transaction
+                reader.Close();
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+            return lstEquips;
+        }
+
+        public static DataTable ListCustomerAssigned()
+        {
+            DataTable dataTable1 = new DataTable();
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            try
+            {
+                sqlConnection.Open();
+                string Query = "spOst_LstCustomerEquiAssignment";
+                SqlCommand cmd = new SqlCommand(Query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.CommandTimeout = 0;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);    //fetch mode table data  
+                adapter.Fill(dataTable1);
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+            return dataTable1;
+        }
+        public bool SaveEquipment()
+        {
+            bool status = false;
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            try
+            {
+                sqlConnection.Open();
+                string Query = "spOST_InsEquipment";
+                SqlCommand cmd = new SqlCommand(Query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add(new SqlParameter("@Name", this.EquipmentName));
+                cmd.Parameters.Add(new SqlParameter("@EcCount", this.Quantity));
+                cmd.Parameters.Add(new SqlParameter("@EntryDate", this.EntryDate));
+                cmd.Parameters.Add(new SqlParameter("@ReceiveDate", this.ReceiveDate));
+                cmd.CommandTimeout = 0;
+
+                int returnvalue = cmd.ExecuteNonQuery();//insert delete update
+                if (returnvalue > 0)
+                {
+                    status = true;
+                }
+
+                //transaction
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+            return status;
+        }
+        public bool UpdateEquipment(string OpType)
+        {
+            bool status = false;
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            try
+            {
+                sqlConnection.Open();
+                string Query = "spOST_UpdEquipment";
+                SqlCommand cmd = new SqlCommand(Query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                if (OpType == "Update")
+                {
+                    cmd.Parameters.Add(new SqlParameter("@EquipmentId", this.EquipmentId));
+                    cmd.Parameters.Add(new SqlParameter("@Name", this.EquipmentName));
+                    cmd.Parameters.Add(new SqlParameter("@EcCount", this.Quantity));
+                    cmd.Parameters.Add(new SqlParameter("@EntryDate", this.EntryDate));
+                    cmd.Parameters.Add(new SqlParameter("@ReceiveDate", this.ReceiveDate));
+                    cmd.Parameters.Add(new SqlParameter("@OpType", OpType));
+                }
+                if (OpType == "Delete")
+                {
+                    cmd.Parameters.Add(new SqlParameter("@EquipmentId", this.EquipmentId));
+                    cmd.Parameters.Add(new SqlParameter("@OpType", OpType));
+                }
+                cmd.CommandTimeout = 0;
+
+                int returnvalue = cmd.ExecuteNonQuery();//insert delete update
+                if (returnvalue > 0)
+                {
+                    status = true;
+                }
+
+                //transaction
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+                throw new ArgumentOutOfRangeException(ex.Message, ex);
+            }
+            return status;
+        }
+        public bool SaveCustomerEquipmentAssignment(FormCollection formcoll)
+        {
+            bool status = false;
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            try
+            {
+                sqlConnection.Open();
+                string Query = "spOST_InsEquiAssignment";
+                SqlCommand cmd = new SqlCommand(Query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add(new SqlParameter("@CustomerID", Convert.ToInt32(formcoll["ddlCustomer"].ToString())));
+                cmd.Parameters.Add(new SqlParameter("@EquipmentID", Convert.ToInt32(formcoll["ddlEquipment"].ToString())));
+                cmd.Parameters.Add(new SqlParameter("@EquiCount", Convert.ToInt32(formcoll["txtEquiCount"].ToString())));
+                cmd.CommandTimeout = 0;
+
+                int returnvalue = cmd.ExecuteNonQuery();//insert delete update
+                if (returnvalue > 0)
+                {
+                    status = true;
+                }
+
+                //transaction
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+            return status;
+        }
+
     }
 }
